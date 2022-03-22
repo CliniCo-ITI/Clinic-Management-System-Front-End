c0659af17ce6a3decfb2a61d19eee90384d8d755
Amr Elauoty <amr.ehab.elauoty@gmail.com>
2022-03-22 22:05:37 +0200

    Implemented models

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,16 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-
-
-
+import {PasswordModule} from 'primeng/password';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import {DividerModule} from 'primeng/divider'
 @NgModule({
   declarations: [
     HeaderComponent,
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,PasswordModule,BrowserAnimationsModule,FormsModule,DividerModule
   ],
   exports:[
     HeaderComponent,
