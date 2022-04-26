5a60fd1d8a55c41acc2341fa7cc7da6ecf967730
abdoelrhmanelwsef <98185334+abdoelrhmanelwsef@users.noreply.github.com>
2022-04-27 01:37:15 +0200

    Update Receptionst Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,25 +9,27 @@ import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
   {
-    path:'login',
-    component:LoginComponent
+    path: 'login',
+    component: LoginComponent
   },
   {
-    path:"invoices",
-    component:InvoiceListComponent
+    path: "invoices",
+    component: InvoiceListComponent
   },
-  { path: "receptionst"
-  ,
-   component:ProfileComponent },
-{
-    path:'register',
-    component:RegisterComponent
+  {
+    path: "receptionst"
+    ,
+    component: ProfileComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
 
-exports: [RouterModule]
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
